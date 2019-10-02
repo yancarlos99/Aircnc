fc03c49f3f91059ce6fb3c8f0c800306a56f87ab
PabloMelo11 <pablomelo418@gmail.com>
2019-10-02 16:18:58 -0300

    Add SpotController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const SessionController = require("./app/controllers/SessionController");
+const SpotController = require("./app/controllers/SpotController");
 
 const routes = express.Router();
 
 routes.post("/sessions", SessionController.store);
+routes.post("/spots", SpotController.store);
 
 module.exports = routes;
