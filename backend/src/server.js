7ef6ae0483da597448a83544dbc6778045ec9840
PabloMelo11 <pablomelo418@gmail.com>
2019-10-02 11:01:27 -0300

    Add conection mongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const routes = require("./routes");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omni:omni@cluster0-nqbor.mongodb.net/omni?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json());
 app.use(routes);
