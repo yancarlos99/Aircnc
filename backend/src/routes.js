568578d9dda37450b9e6fb8c3c903aa630351e3a
PabloMelo11 <pablomelo418@gmail.com>
2019-10-02 16:26:58 -0300

    Add multerConfig

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const SessionController = require("./app/controllers/SessionController");
 const SpotController = require("./app/controllers/SpotController");
 
