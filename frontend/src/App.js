fc02d0d60aa02ca3bd3026c1ad70cea8b8edb754
PabloMelo11 <pablomelo418@gmail.com>
2019-10-03 01:08:34 -0300

    Add cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="Aircnc"/>
@@ -11,12 +20,14 @@ function App() {
       <div className="content">
         <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL</label>
           <input 
             type="email" 
             id="email" 
-            placeholder="Seu melhor e-mail" 
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
 
           <button className="btn" type="submit">ENTRAR</button>
