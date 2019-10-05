7a7c323093ae5d46cc83fa00b051cd747b9b5e43
PabloMelo11 <pablomelo418@gmail.com>
2019-10-05 16:43:46 -0300

    Config connection backend and frontend

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import api from '../../services/api';
 import './styles.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const user_id = localStorage.getItem('user');
+    const socket = socketio('http://192.168.0.106:3333', {
+      query: { user_id }
+    })
+
+    socket.on('booking_request', data => {
+      console.log(data);
+    })
+  }, [])
 
   useEffect(() => {
     async function loadSpots() {
