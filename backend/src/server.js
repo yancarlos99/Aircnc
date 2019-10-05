b7869b1af4b039ffd1e3bfe0908680881bd161e5
PabloMelo11 <pablomelo418@gmail.com>
2019-10-05 16:06:56 -0300

    Add socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
 const routes = require("./routes");
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on("connection", socket => {
+  console.log("Usuario conectado", socket.id);
+});
 
 mongoose.connect(
   "mongodb+srv://omni:omni@cluster0-nqbor.mongodb.net/omni?retryWrites=true&w=majority",
@@ -19,4 +28,4 @@ app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
