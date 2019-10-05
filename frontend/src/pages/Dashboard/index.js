1cb81a46c91821ab002ac17896ddcadd2d5ac87a
PabloMelo11 <pablomelo418@gmail.com>
2019-10-05 16:58:21 -0300

    Add requests in frontend

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import socketio from 'socket.io-client';
 import api from '../../services/api';
@@ -6,17 +6,18 @@ import './styles.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
+
+  const user_id = localStorage.getItem('user');
+    const socket = useMemo(() => socketio('http://192.168.0.106:3333', {
+      query: { user_id }
+    }), [user_id]);
 
   useEffect(() => {
-    const user_id = localStorage.getItem('user');
-    const socket = socketio('http://192.168.0.106:3333', {
-      query: { user_id }
-    })
-
     socket.on('booking_request', data => {
-      console.log(data);
+      setRequests([...requests, data])
     })
-  }, [])
+  }, [requests, socket])
 
   useEffect(() => {
     async function loadSpots() {
@@ -33,6 +34,20 @@ export default function Dashboard() {
 
   return (
     <>
+  <ul className="notifications">
+    {requests.map(request => (
+      <li key={request._id}>
+        <p>
+          <strong>{request.user.email}</strong> esta solicitando uma reserva em 
+          <strong>{}request.spot.company</strong> 
+          para a data: <strong>{request.date}</strong>
+        </p>
+        <button className="accept">ACEITAR</button>
+        <button className="reject">REJEITAR</button>
+      </li>
+    ))}
+  </ul>
+
       <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
