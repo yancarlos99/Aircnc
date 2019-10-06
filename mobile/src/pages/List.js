b9ad8815e9eecf2b7278702534a294529f21c705
PabloMelo11 <pablomelo418@gmail.com>
2019-10-06 00:05:45 -0300

    App finish

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import socketio from "socket.io-client";
 import {
   SafeAreaView,
   StyleSheet,
   Image,
   AsyncStorage,
-  ScrollView
+  ScrollView,
+  Alert
 } from "react-native";
 
 import SpotList from "../components/SpotList";
@@ -13,6 +15,22 @@ import logo from "../assets/logo.png";
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("user").then(user_id => {
+      const socket = socketio("http://192.168.0.106:3333", {
+        query: { user_id }
+      });
+
+      socket.on("booking_response", booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? "APROVADA" : "REJEITADA"
+          }`
+        );
+      });
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem("techs").then(storagedTechs => {
